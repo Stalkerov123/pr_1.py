224c8a540bfb60bcb57a6b3d767c77a541d1e629
Dmitri <68437321+Stalkerov123@users.noreply.github.com>
2022-09-17 19:24:19 +0300

    Update code_1.py

---
+++ code_1.py
@@ -0,0 +1,5 @@
+full_name = input('Напишите ваше полное ФИО: \n')
+age = input("Сколько вам полных лет? \n")
+place_of_living = input('Где вы живёте?(только город) \n')
+
+print(f" Ваше ФИО: {full_name},\n Ваш возраст {age},\n Ваше место жительства: {place_of_living}")
